568ab5765c24cdd8c6bfc6a8701dd21b24050371
Farman Shaik <shkfrmn786@gmail.com>
2025-03-28 13:42:46 +0530

    finish

---
+++ utils/helper.js
@@ -0,0 +1,3 @@
+import { Connection } from '@solana/web3.js';
+
+export const connection = new Connection("https://mainnet.helius-rpc.com/?api-key=6f17ef70-139f-463a-bfaa-85a120eee8d3", "confirmed");
